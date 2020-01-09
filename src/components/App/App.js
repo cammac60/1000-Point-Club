bb2cf5f3d3a75d8799e85b250fcb9166833f5b41
Cameron MacRae <cam.mac.cm@gmail.com>
2020-01-09 12:28:12 -0700

    Style header component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,11 +4,13 @@ import { Route } from 'react-router-dom';
 import './App.scss';
 
 import Splash from '../../containers/Splash/Splash.js';
+import { Header } from '../Header/Header';
 
 const App = () => {
   return (
     <div className="App">
       <Route exact path="/" render={() => <Splash />} />
+      <Route exact path="/profile" render={() => <Header />}/>
     </div>
   );
 }
