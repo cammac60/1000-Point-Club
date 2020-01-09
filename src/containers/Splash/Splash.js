246cae42ca7970e7b65e5e7a4a8413fda460cc11
Cameron MacRae <cam.mac.cm@gmail.com>
2020-01-08 21:16:41 -0700

    Creat and style splash page

--- src/containers/Splash/Splash.js
+++ src/containers/Splash/Splash.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import './Splash.scss';
+
 class Splash extends Component {
   constructor() {
     super();
@@ -10,11 +12,44 @@ class Splash extends Component {
   }
   render = () => {
     return (
-      <form className="sign-in-form">
-      <img alt="NHL Logo" className="nhl-logo-lrg" src={require("../../images/logos/NHL_Logo.png")}/>
-      <h1>1000 Point Club</h1>
-      </form>
+      <div className="sign-in-wrapper">
+        <form className="sign-in-form">
+          <img alt="NHL Logo" className="nhl-logo-lrg" src={require("../../images/logos/NHL_Logo.png")}/>
+          <h1>1000 Point Club</h1>
+          <label className="sign-in-label">Enter your player ID:</label>
+          <input className="sign-in-input" type="number" value={this.state.id} onChange={(event) => this.handleChange(event)}/>
+          <button className="sign-in-btn" onClick={(event) => this.handleSubmit(event)}>Sign In</button>
+          <span className="sign-in-error">{this.state.error}</span>
+        </form>
+      </div>
     )
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      id: event.target.value,
+      error: ''
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    if (this.state.id) {
+      this.fetchUser(this.state);
+    } else {
+        this.setState({error: 'Please enter an ID'});
+    }
+  }
+
+  fetchUser = async ({ id }) => {
+    let baseInfoURL = `https://statsapi.web.nhl.com/api/v1/people/${id}`;
+    let statsURL = `${baseInfoURL}/stats?stats=careerRegularSeason`;
+    try {
+      
+    }
+    catch {
+
+    }
   }
 }
 
