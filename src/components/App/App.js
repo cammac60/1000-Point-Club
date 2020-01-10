83f92d07ad5cc51e942d710ba95c94bd6d348c60
Cameron MacRae <cam.mac.cm@gmail.com>
2020-01-10 14:22:46 -0700

    Refactor club fetching

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { getThousandClub, getUserInfo } from '../../apiCalls/apiCalls';
+import { addMembers } from '../../actions';
 
 import './App.scss';
 
@@ -10,29 +14,91 @@ import Loader from '../Loader/Loader';
 import Profile from '../Profile/Profile';
 import Club from '../../containers/Club/Club';
 
-const App = props => {
-  return (
-    <div className="App">
-      <Route exact path="/" render={() => <Splash />} />
-      <Route exact path="/profile" render={() =>
-        props.user ?
-        <>
-          <Header />
-          <Profile />
-        </> :
-        <Loader />
-      }
-      />
-      <Route exact path="/1000club" render={() =>
-        props.members ?
-        <>
-          <Header />
-          <Club />
-        </> :
-        <Loader />}
-      />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render = () => {
+    return (
+      <div className="App">
+        <Route exact path="/" render={() => <Splash />} />
+        <Route exact path="/profile" render={() =>
+          this.props.user ?
+          <>
+            <Header />
+            <Profile />
+          </> :
+          <Loader />
+        }
+        />
+        <Route exact path="/1000club" render={() =>
+          this.props.members ?
+          <>
+            <Header />
+            <Club />
+          </> :
+          <Loader />}
+        />
+      </div>
+    );
+  }
+
+  componentDidMount = () => {
+    this.fetchClub();
+  }
+
+  fetchClub = async () => {
+    try {
+      const clubMems = await getThousandClub('https://records.nhl.com/site/api/milestone-1000-point-career');
+      let memIDs = clubMems.data.map(mem => mem.id.skaterId);
+      let memberInfo = await this.createMembers(memIDs);
+      this.props.addMembers(memberInfo);
+    }
+    catch (error){
+      console.log(error);
+    }
+  }
+
+  createMembers  = (members) => {
+    let memberList = [];
+    members.forEach(async mem => {
+      let memberObj = await this.fetchUser(mem);
+      memberList.push(memberObj);
+    });
+    return memberList
+  }
+
+  fetchUser = async (id) => {
+    let baseInfoURL = `https://statsapi.web.nhl.com/api/v1/people/${id}`;
+    let statsURL = `${baseInfoURL}/stats?stats=careerRegularSeason`;
+    try {
+      const basicUserInfo = await getUserInfo(baseInfoURL);
+      const userStats = await getUserInfo(statsURL);
+      const user = await this.createUser(basicUserInfo, userStats);
+      return user
+    }
+    catch (error) {
+      this.setState({error: error.message});
+    }
+  }
+
+  createUser = (basicUserInfo, userStatsInfo) => {
+    let basicData = basicUserInfo.people[0];
+    let userCareerStats = userStatsInfo.stats[0].splits[0].stat;
+    let user = {
+      id: basicData.id,
+      name: basicData.fullName,
+      birthDate: basicData.birthDate,
+      birthCity: basicData.birthCity,
+      birthStateProvince: basicData.birthStateProvince,
+      birthCountry: basicData.birthCountry,
+      position: basicData.primaryPosition.code,
+      stats: userCareerStats
+    };
+    return user;
+  }
+
 }
 
 export const mapStateToProps = state => ({
@@ -40,4 +106,10 @@ export const mapStateToProps = state => ({
   members: state.members
 });
 
-export default connect(mapStateToProps)(App);
+export const mapDispatchToProps = dispatch => (
+  bindActionCreators({
+    addMembers
+  }, dispatch)
+);
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,14 +2,12 @@ import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { removeUser, addMembers } from '../../actions';
-import { getThousandClub, getUserInfo } from '../../apiCalls/apiCalls';
+import { removeUser } from '../../actions';
 import './Header.scss';
 
 export class Header extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
   }
 
   render = () => {
@@ -18,7 +16,7 @@ export class Header extends Component {
         <img src={require('../../images/logos/NHL_Logo.png')} alt="NHL Logo" className="header-logo"/>
         <nav className="header-links-wrapper">
           <Link to="/profile" className="nav-link">Your Stats</Link>
-          <Link to="/1000club" className="nav-link" onClick={this.fetchClub}>1000 Point Club</Link>
+          <Link to="/1000club" className="nav-link">1000 Point Club</Link>
           <Link to="/search" className="nav-link">Search Players</Link>
         </nav>
         <button className="sign-out-button" onClick={this.logOut}>Sign Out ></button>
@@ -31,63 +29,11 @@ export class Header extends Component {
     this.props.history.push('/');
   }
 
-  fetchClub = async () => {
-    try {
-      const clubMems = await getThousandClub('https://records.nhl.com/site/api/milestone-1000-point-career');
-      let memIDs = clubMems.data.map(mem => mem.id.skaterId);
-      let memberInfo = await this.createMembers(memIDs);
-      this.props.addMembers(memberInfo);
-    }
-    catch (error){
-      console.log(error);
-    }
-  }
-
-  createMembers  = (members) => {
-    let memberList = [];
-    members.forEach(async mem => {
-      let memberObj = await this.fetchUser(mem);
-      memberList.push(memberObj);
-    });
-    return memberList
-  }
-
-  fetchUser = async (id) => {
-    let baseInfoURL = `https://statsapi.web.nhl.com/api/v1/people/${id}`;
-    let statsURL = `${baseInfoURL}/stats?stats=careerRegularSeason`;
-    try {
-      const basicUserInfo = await getUserInfo(baseInfoURL);
-      const userStats = await getUserInfo(statsURL);
-      const user = await this.createUser(basicUserInfo, userStats);
-      return user
-    }
-    catch (error) {
-      this.setState({error: error.message});
-    }
-  }
-
-  createUser = (basicUserInfo, userStatsInfo) => {
-    let basicData = basicUserInfo.people[0];
-    let userCareerStats = userStatsInfo.stats[0].splits[0].stat;
-    let user = {
-      id: basicData.id,
-      name: basicData.fullName,
-      birthDate: basicData.birthDate,
-      birthCity: basicData.birthCity,
-      birthStateProvince: basicData.birthStateProvince,
-      birthCountry: basicData.birthCountry,
-      position: basicData.primaryPosition.code,
-      stats: userCareerStats
-    };
-    return user;
-  }
-
 }
 
 export const mapDispatchToProps = dispatch => (
   bindActionCreators({
-    removeUser,
-    addMembers
+    removeUser
   }, dispatch)
 );
 
