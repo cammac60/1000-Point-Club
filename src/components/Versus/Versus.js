03b3ef263ce422b222e379e53ca13071593f733b
Cameron MacRae <cam.mac.cm@gmail.com>
2020-01-11 11:33:03 -0700

    Add versus files

---
+++ src/components/Versus/Versus.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './Versus.scss';
+
+export const Versus = (props) => {
+  console.log(props.user);
+  console.log(props.selected);
+  return (
+    <div className="versus-wrapper">
+    </div>
+  )
+}
