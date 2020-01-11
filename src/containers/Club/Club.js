75374d79fa62a334194af27ce8826f3de0252ef1
Cameron MacRae <cam.mac.cm@gmail.com>
2020-01-10 18:28:27 -0700

    Add card styles

--- src/containers/Club/Club.js
+++ src/containers/Club/Club.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import './Club.scss';
 
@@ -8,22 +9,46 @@ export class Club extends Component {
     super(props);
   }
 
-
   render = () => {
-    console.log(this.props.members);
-    let cards = this.props.members.map(mem => {
+    let sortedMembers = this.sortMembers(this.props.members);
+
+    let cards = this.props.members.map((mem, i) => {
+      let hometown;
+      mem.birthStateProvince ?
+      hometown = `${mem.birthCity}\, ${mem.birthStateProvince}\, ${mem.birthCountry}` :
+      hometown = `${mem.birthCity}\, ${mem.birthCountry}`;
       return (
         <section className="legend-card">
-          {mem.id}
+          <h3 className="rank-num">{i + 1}</h3>
+          <section className="player-info">
+            <h3 className="player-name">{mem.name}</h3>
+            <div className="player-info-small-wrap">
+              <span className="info-label">Birthplace:   </span>
+              <span className="player-info-data">{hometown}<img className="flag-icon" src={require(`../../images/flags/${mem.birthCountry}.png`)} alt="Country Flag"/></span>
+            </div>
+            <div className="player-info-small-wrap">
+              <span className="info-label">Birthdate:   </span>
+              <span className="player-info-data">{mem.birthDate}</span>
+            </div>
+            <Link className="versus-link" to="/vs" id={mem.id}>You vs. Him</Link>
+          </section>
+          <section className="player-stats">
+          </section>
         </section>
       )
     });
-    console.log(cards);
+
     return (
       <div className="club-wrapper">
         {cards}
       </div>
     )
+  }
+
+  sortMembers = (members) => {
+    return members.sort((a, b) => {
+      return b.stats.points - a.stats.points
+    });
   }
 
 }
