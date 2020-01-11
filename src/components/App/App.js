b0767be357811081b95b4c723afea00dc9fd4f9e
Cameron MacRae <cam.mac.cm@gmail.com>
2020-01-11 11:32:27 -0700

    Add versus and minor style changes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -14,6 +14,7 @@ import Loader from '../Loader/Loader';
 import Profile from '../Profile/Profile';
 import Club from '../../containers/Club/Club';
 import { CheatSheet } from '../CheatSheet/CheatSheet';
+import { Versus } from '../Versus/Versus';
 
 export class App extends Component {
   constructor(props) {
@@ -42,6 +43,7 @@ export class App extends Component {
           <Loader />}
         />
         <Route exact path="/cheat-sheet" render={() => <CheatSheet />}/>
+        <Route exact path="/vs" render={() => <Versus user={this.props.user} selected={this.props.selected}/>}/>
       </div>
     );
   }
@@ -105,7 +107,8 @@ export class App extends Component {
 
 export const mapStateToProps = state => ({
   user: state.user,
-  members: state.members
+  members: state.members,
+  selected: state.selected
 });
 
 export const mapDispatchToProps = dispatch => (
