48c4446d69b43d5fc7c56a24409b4b76f9c2872b
Cameron MacRae <cam.mac.cm@gmail.com>
2020-01-12 11:39:38 -0700

    Style roster page

--- src/containers/RosterSearch/RosterSearch.js
+++ src/containers/RosterSearch/RosterSearch.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { bindActionCreators } from 'redux';
 import { addSelectedPlayer } from '../../actions';
 
@@ -11,8 +12,11 @@ export class RosterSearch extends Component {
   }
 
   render = () => {
-    let team = this.props.roster;
-    let players = team.roster.map(player => {
+    let curRoster = this.props.roster;
+    let team = this.props.teams.find(team => team.id === parseInt(this.props.roster.id));
+    let logoURL = team.abbreviation;
+    let teamName = `${team.name} Roster`;
+    let players = curRoster.roster.map(player => {
       return (
         <tr key={player.id} className="roster-tbl-row">
           <td className="roster-tbl-data">
@@ -25,15 +29,19 @@ export class RosterSearch extends Component {
             {player.position}
           </td>
           <td className="roster-tbl-data">
-            <button id={player.id} className="roster-ply-select">
+            <Link id={player.id} className="roster-ply-select" to="/vs" onClick={(event) => this.addToSelected(event)}>
             You vs. Him
-            </button>
+            </Link>
           </td>
         </tr>
       )
     });
     return (
       <div className="roster-search-wrapper">
+        <div className="roster-head-wrapper">
+          <img className="roster-team-logo" alt={`${logoURL} Logo`} src={require(`../../images/logos/${logoURL}.svg`)}/>
+          <h2 className="roster-header">{teamName}</h2>
+        </div>
         <table className="roster-tbl">
           <thead>
             <tr className="roster-tbl-row">
@@ -59,6 +67,12 @@ export class RosterSearch extends Component {
     )
   }
 
+  addToSelected = async ({ target }) => {
+    let { id } = target;
+    let player = await this.props.fetchUser(id);
+    this.props.addSelectedPlayer(player);
+  }
+
 }
 
 export const mapDispatchToProps = dispatch => (
@@ -69,7 +83,8 @@ export const mapDispatchToProps = dispatch => (
 
 export const mapStateToProps = state => ({
   selected: state.selected,
-  roster: state.roster
+  roster: state.roster,
+  teams: state.teams
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(RosterSearch);
