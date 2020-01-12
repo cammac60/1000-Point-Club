9cef9b3d1599415b2266a7c79e00112353436af4
Cameron MacRae <cam.mac.cm@gmail.com>
2020-01-12 00:48:10 -0700

    Add team filter tests

---
+++ src/components/TeamFilter/TeamFilter.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { TeamFilter } from './TeamFilter.js';
+
+describe('TeamFilter', () => {
+  let wrapper, instance;
+  const selectRoster = jest.fn();
+  const mockProps = {
+    teams: [{
+      abbreviation: 'COL',
+      id: 1,
+      name: 'name'
+    },
+    {
+      abbreviation: 'COL',
+      id: 2,
+      name: 'name'
+    }],
+    selectRoster
+  };
+  const mockEvent = {
+    terget: {
+      id: 1
+    }
+  };
+
+  beforeEach(() => {
+    wrapper = shallow(<TeamFilter {...mockProps}/>);
+    instance = wrapper.instance();
+  });
+
+  it('Should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it.skip('Should call the selectRoster method on click of a button with the correct agrument', () => {
+    wrapper.find('.team-btn').first().simulate('click', mockEvent);
+    expect(wrapper.props.selectRoster).toHaveBeenCalledWith(mockEvent);
+  });
+
+});
